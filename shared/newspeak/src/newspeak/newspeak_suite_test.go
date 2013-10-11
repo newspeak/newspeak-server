76c8d45b7fec9744c9b6542d68bd8217441d425b
newspeak <dev@newspeak.io>
2013-10-11 13:32:50 +0200

    add gocheck benchmark

--- shared/newspeak/src/newspeak/newspeak_suite_test.go
+++ shared/newspeak/src/newspeak/newspeak_suite_test.go
@@ -11,7 +11,18 @@ import (
 // hook up gocheck into the "go test" runner.
 func Test(t *testing.T) { TestingT(t) }
 
+type NewspeakSuite struct{}
+var _ = Suite(&NewspeakSuite{})
+
+// unit tests
 func TestNewspeak(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Newspeak Suite")
+}
+
+// benchmarks
+func (s *NewspeakSuite) BenchmarkLogic(c *C) {
+	for i := 0; i < c.N; i++ {
+		// logic to benchmark
+	}
 }
