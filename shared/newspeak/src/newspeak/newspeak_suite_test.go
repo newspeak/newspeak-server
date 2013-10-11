9470bb1ecafe75b22bfcf8d69733dc3dd7ab162c
newspeak <dev@newspeak.io>
2013-10-11 13:32:12 +0200

    use gocheck

--- shared/newspeak/src/newspeak/newspeak_suite_test.go
+++ shared/newspeak/src/newspeak/newspeak_suite_test.go
@@ -3,9 +3,13 @@ package newspeak_test
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	. "launchpad.net/gocheck"
 
 	"testing"
 )
+
+// hook up gocheck into the "go test" runner.
+func Test(t *testing.T) { TestingT(t) }
 
 func TestNewspeak(t *testing.T) {
 	RegisterFailHandler(Fail)
